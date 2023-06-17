988cd4373e9a55f7f9b612547ba83236c3d76224
fiskryeziu <fiskryeziu@gmail.com>
2023-06-17 22:00:46 +0200

    add helper function, (testing)

---
+++ src/pages/api/helpers/dateChecker.ts
@@ -0,0 +1,24 @@
+export function isGreaterThanMonth(date1: string, date2: string): boolean {
+    // One month to milliseconds
+    const monthMillis = 30 * 24 * 60 * 60 * 1000;
+
+    // Parse the input strings into Date objects
+    const parsedDate1 = new Date(date1);
+    const parsedDate2 = new Date(date2);
+
+    // Calculate the difference between the two dates in milliseconds
+    const difference = Math.abs(parsedDate2.getTime() - parsedDate1.getTime());
+
+    // Check if the difference is greater than or equal to one month in milliseconds
+    if (difference >= monthMillis) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Example usage
+const date1 = "2023-05-15";
+const date2 = "2023-06-16";
+const result = isGreaterThanMonth(date1, date2);
+console.log(result); // Output: true
