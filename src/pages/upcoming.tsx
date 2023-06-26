2a7a85ff48705b2eb38e5e3749390ce19b45a90d
fiskryeziu <fiskryeziu@gmail.com>
2023-06-26 20:00:32 +0200

    responsive upcoming page

--- src/pages/upcoming.tsx
+++ src/pages/upcoming.tsx
@@ -20,18 +20,20 @@ const UpcomingTodoPage = () => {
   }
   return (
     <main className="flex grow flex-col items-center gap-2">
-      <div className="my-10 flex w-1/2 justify-center">
-        <p className="text-center text-xl tracking-wider text-white">
-          Upcoming
-        </p>
-      </div>
-      <div className="relative flex w-1/2 flex-col gap-4">
-        {todos?.map((todo) => (
-          <TodoItem todo={todo} key={todo.id} />
-        ))}
-      </div>
+      <div className="flex w-full flex-col items-center md:w-3/4 lg:w-1/2">
+        <div className="my-10">
+          <p className="text-center text-xl tracking-wider text-white">
+            Upcoming
+          </p>
+        </div>
+        <div className="relative flex w-full  flex-col gap-4">
+          {todos?.map((todo) => (
+            <TodoItem todo={todo} key={todo.id} />
+          ))}
+        </div>
 
-      {todos === undefined || (todos.length === 0 && <Empty />)}
+        {todos === undefined || (todos.length === 0 && <Empty />)}
+      </div>
     </main>
   );
 };
